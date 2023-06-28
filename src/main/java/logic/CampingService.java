6fa25679cc551262e6482d98b172c964cfc2d114
kus <kus74@DESKTOP-AGRJQGJ>
2023-06-28 16:27:18 +0900

    .

---
+++ src/main/java/logic/CampingService.java
@@ -0,0 +1,33 @@
+package logic;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import dao.CampDao;
+
+@Service
+public class CampingService {
+	@Autowired
+	private CampDao campDao;
+	
+	public List<Camp> camplist(Map<String, Object> param) {
+		return campDao.list(param);
+	}
+
+	public int campcount(Map<String, Object> param) {
+		return campDao.count(param);
+	}
+
+	public int campcount2(String themelist, String pet, String aroundlist) {
+		return campDao.count2(themelist,pet,aroundlist);
+	}
+
+	public List<Camp> camplist2(String themelist, String pet, String aroundlist, Integer pageNum, int limit,
+			int startrow) {
+		return campDao.list2(themelist,pet,aroundlist,pageNum,limit,startrow);
+	}
+
+}
